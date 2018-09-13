329412e2e6b3cfdc4465fc95bb30e8a726917436
gifersasi <sasi.gifer@gmail.com>
2018-09-13 10:43:35 +0700

    cp1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CharacterCard from './CharacterCard';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcasdome tojs React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      
+        <CharacterCard value="H"/>
+        <CharacterCard value="e"/>
+        <CharacterCard value="l"/>
+        <CharacterCard value="l"/>
+        <CharacterCard value="o"/>
+      
       </div>
     );
   }
