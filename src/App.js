140e28b1967d1c3c44ba68ab73b1531b2abca521
gifersasi <sasi.gifer@gmail.com>
2018-09-13 11:09:17 +0700

    cp2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import CharacterCard from './CharacterCard';
 const word = "Hello";
 class App extends Component {
  render() {
