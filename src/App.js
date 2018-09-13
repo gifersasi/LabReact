8ab019f72a4266692814b0927faa4ac9688de6d4
gifersasi <sasi.gifer@gmail.com>
2018-09-13 10:57:28 +0700

    cp1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import CharacterCard from './CharacterCard';
-
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-       Hello World
-      
-      </div>
-    );
-  }
+ render() {
+ return (
+ <div className="App">
+ Hello World
+ </div>
+ );
+ }
 }
-
-export default App;
+export default App
